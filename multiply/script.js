0fa9bc6182827a56bbc312700169d02f96a03fee
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-03-22 23:20:55 +0100

    Create script.js

---
+++ multiply/script.js
@@ -0,0 +1,7 @@
+ //calls random numbers
+//*10 gives numbers between 0 and 10
+//Math.ceil makes it whole number
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+}
