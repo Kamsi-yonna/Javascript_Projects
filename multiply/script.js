3e416d179c70e8684272a228fb34634ffcea08b7
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-03-23 07:49:32 +0100

    Update script.js

--- multiply/script.js
+++ multiply/script.js
@@ -1,33 +1,37 @@
- //calls random numbers
-//*10 gives numbers between 0 and 10
-//Math.ceil makes it whole number
 const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
-//We want to manipulate the header
-//We will bring it to the JS with get element
-const questionE1 = document.getElementById("question");
-const inputE1 = document.getElementById('input');
-const formE1 = document.getElementById('form');
+const questionEl = document.getElementById("question");
 
-let score = 0;
-questionE1.innerText = `What is ${num1} times ${num2}?`;
-// console.log(num1);
-//to get the answer
-const solution = num1 * num2;
-formE1.addEventListener('submit', ()=>{
+const inputEl = document.getElementById("input");
 
-    const userSolution = +inputE1.value;
-    // console.log(userSolution, typeof userSolution);
-    if (userSolution === solution){
-        score++;
-        console.log(score);
-    } else {
-        score--;
-        console.log(score);
-    }
-})
+const formEl = document.getElementById("form");
 
-function UpdateLocalstorage(){
-    localstorage.setItem("score", JSON.stringify(score))
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+  score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage();
+  } else {
+    score--;
+    updateLocalStorage();
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
 }
