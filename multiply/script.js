6ea5d6514714e7dc16abd93bcc03abcdb558485d
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-03-25 04:36:47 +0100

    Update script.js

--- multiply/script.js
+++ multiply/script.js
@@ -1,9 +1,6 @@
 const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
-const questionEl = document.getElementById("question");
-
-const inputEl = document.getElementById("input");
 
 const formEl = document.getElementById("form");
 
