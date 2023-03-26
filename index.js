11e9b157246ee277ba36c678432e73d1bd0e1688
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-03-26 22:56:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const btnEl = document.getElementById("btn");
-const animeContainerEl = document.querySelector(".anime-container");
 const animeImgEl = document.querySelector(".anime-img");
-const amineNameEl = document.querySelector(".anime-name");
 
 btnEl.addEventListener("click", async function () {
   try {
     btnEl.disabled = true;
     btnEl.innerText = "Loading...";
     const data = await response.json();
-    btnEl.disabled = false;
-    btnEl.innerText = "Get Anime";
-    animeContainerEl.style.display = "block";
     animeImgEl.src = data.url;
-    amineNameEl.innerText = data.artist;
   } catch (error) {
     console.log(error);
     btnEl.disabled = false;
