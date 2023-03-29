66dd52f78732b52c393916f36bcc0bdf2ba0deaa
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-03-29 04:18:08 +0100

    Update index.js

--- amine-pics-generator/index.js
+++ amine-pics-generator/index.js
@@ -1,15 +1,12 @@
 const btnEl = document.getElementById("btn");
-const animeContainerEl = document.querySelector(".anime-container");
 const animeImgEl = document.querySelector(".anime-img");
 const amineNameEl = document.querySelector(".anime-name");
 
 btnEl.addEventListener("click", async function () {
-  try {
     btnEl.disabled = true;
     btnEl.innerText = "Loading...";
     amineNameEl.innerText = "Updating...";
     const response = await fetch("https://api.catboys.com/img");
-    amineNameEl.innerText = data.artist;
     btnEl.disabled = false;
     btnEl.innerText = "Get Anime";
     amineNameEl.innerText = "An error happened, please try again";
