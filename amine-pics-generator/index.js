3def14c4784f7949c1056abfdd2bd5844f84771d
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-03-30 15:44:58 +0100

    Delete amine-pics-generator directory

--- amine-pics-generator/index.js
+++
@@ -1,7 +0,0 @@
-const btnEl = document.getElementById("btn");
-    amineNameEl.innerText = "Updating...";
-    const response = await fetch("https://api.catboys.com/img");
-    btnEl.innerText = "Get Anime";
-    amineNameEl.innerText = "An error happened, please try again";
-  }
-});
