e3017783afa830524e669ec9161498f43f897a38
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-04-02 10:05:06 +0100

    Update script.js

--- Color_Generator/script.js
+++ Color_Generator/script.js
@@ -9,3 +9,22 @@ for (let index = 0; index < 30; index++) {
 const colorContainerEls = document.querySelectorAll(".color-container");
 
 generateColors();
+
+function generateColors() {
+  colorContainerEls.forEach((colorContainerEl) => {
+    const newColorCode = randomColor();
+    colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    colorContainerEl.innerText = "#" + newColorCode;
+  });
+}
+
+function randomColor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
+  let colorCode = "";
+  for (let index = 0; index < colorCodeLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNum, randomNum + 1);
+  }
+  return colorCode;
+}
